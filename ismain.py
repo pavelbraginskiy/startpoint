d2ad6a9e8e3b0ed2101c5b96c5f257f37341b392
Pavel Braginskiy <pavel@appesteem.com>
2018-02-08 07:56:22 -0800

    Support argv

--- ismain.py
+++ ismain.py
@@ -1,5 +1,16 @@
+from inspect import signature
+
 def entrypoint(modname, alt=lambda:None):
     def _f(func):
-        func() if modname == '__main__' else alt()
+        if modname == "__main__":
+            count = len(signature(func).parameters)
+            if count == 1:
+                func(__import__('sys'.argv))
+            elif count == 0:
+                func()
+            else:
+                raise raise ValueError("Entry point should take 0 or 1 arguments.")
+        else:
+            alt()
         return func
     return _f
