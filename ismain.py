db674b43ec2bce081ff64c141af0a4693ecc9419
Pavel Braginskiy <pavel@appesteem.com>
2018-02-08 08:08:59 -0800

    Add setup.py

--- ismain.py
+++ ismain.py
@@ -3,11 +3,13 @@ from inspect import signature
 def entrypoint(modname, alt=lambda:None):
     def _f(func):
         if modname == "__main__":
-            count = len(signature(func).parameters)
-            if count > 0:
+            count = len([x for x in signature(f).parameters.values() if x._kind is Parameter.POSITIONAL_OR_KEYWORD and x.default is Parameter.empty])
+            if count == 1:
                 func(__import__('sys'.argv))
-            else:
+            elif count == 0:
                 func()
+            else:
+                raise ValueError("Entry point should have 0 or 1 required arguments!")
         else:
             alt()
         return func
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="IsMain",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=['docutils>=0.3'],
+    package_data={
+        '': ['*.txt', '*.rst'],
+    },
+    author="Pavel Braginskiy",
+    author_email="pavelbraginskiy@hotmail.com",
+    description="A better entry point than if __name__ == '__main__'",
+    license="MIT",
+    url="https://github.com/pavelbraginskiy/ismain/tree/master"
+
+)
