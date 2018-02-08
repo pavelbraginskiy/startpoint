4fa432d7cb9d591cc4e615e8fb4c10f42a95b4bf
Pavel Braginskiy <pavel@appesteem.com>
2018-02-08 08:02:18 -0800

    Fix argv passing

--- ismain.py
+++ ismain.py
@@ -4,12 +4,10 @@ def entrypoint(modname, alt=lambda:None):
     def _f(func):
         if modname == "__main__":
             count = len(signature(func).parameters)
-            if count == 1:
+            if count > 0:
                 func(__import__('sys'.argv))
-            elif count == 0:
-                func()
             else:
-                raise raise ValueError("Entry point should take 0 or 1 arguments.")
+                func()
         else:
             alt()
         return func
